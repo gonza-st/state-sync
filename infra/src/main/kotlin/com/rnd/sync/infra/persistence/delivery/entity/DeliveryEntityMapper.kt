2c016df32da8b772cb14b6042f83519a6526cacc
han-0624 <han@wemeetmobility.com>
2025-03-27 01:49:22 +0900

    feat: delivery decomposite 클래스 제거

--- infra/src/main/kotlin/com/rnd/sync/infra/persistence/delivery/entity/DeliveryEntityMapper.kt
+++ infra/src/main/kotlin/com/rnd/sync/infra/persistence/delivery/entity/DeliveryEntityMapper.kt
@@ -1,24 +1,10 @@
 package com.rnd.sync.infra.persistence.delivery.entity
 
-import com.rnd.sync.application.domain.delivery.DeliveryDecomposite
 import com.rnd.sync.application.domain.delivery.Delivery
-import com.rnd.sync.application.domain.deliveryplan.DeliveryPlan.DeliveryPlanId
 import org.springframework.stereotype.Component
 
 @Component
 class DeliveryEntityMapper {
-    fun fromDeliveryDecompositeToNewDeliveryEntity(deliveryDecomposite: DeliveryDecomposite): DeliveryEntity {
-        return DeliveryEntity(
-            orderId = deliveryDecomposite.orderId.id,
-            orderNumber = deliveryDecomposite.orderNumber,
-            destination = deliveryDecomposite.destination,
-            driverName = deliveryDecomposite.driverName,
-            deliveryOrder = deliveryDecomposite.deliveryOrder,
-            status = deliveryDecomposite.status.name(),
-            deliveryPlanId = deliveryDecomposite.deliveryPlanId.id
-        )
-    }
-
     fun fromDeliveryToNewDeliveryEntity(delivery: Delivery, deliveryPlanId: Long): DeliveryEntity {
         return DeliveryEntity(
             orderId = delivery.orderId.id,
@@ -42,23 +28,6 @@ class DeliveryEntityMapper {
             status = delivery.status.name(),
             deliveryPlanId = deliveryPlanId
         )
-    }
-
-    fun fromDeliveryEntityToDeliveryDecomposite(deliveryEntity: DeliveryEntity): DeliveryDecomposite {
-        val deliveryId = deliveryEntity.id ?: throw IllegalArgumentException("Delivery with id ${deliveryEntity.id} not found")
-
-        val deliveryDecomposite = DeliveryDecomposite.create(
-            id = deliveryId,
-            orderId = deliveryEntity.orderId,
-            orderNumber = deliveryEntity.orderNumber,
-            destination = deliveryEntity.destination,
-            driverName = deliveryEntity.driverName,
-            deliveryOrder = deliveryEntity.deliveryOrder,
-            status = deliveryEntity.status,
-        )
-
-        deliveryDecomposite.mapDeliveryPlanId(DeliveryPlanId(deliveryEntity.deliveryPlanId))
-        return deliveryDecomposite
     }
 
     fun fromDeliveryEntityToDelivery(deliveryEntity: DeliveryEntity): Delivery {
